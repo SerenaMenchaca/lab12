f6efea2e2f7c71b0af1ade07ba52acb0edcbaf0a
SerenaMenchaca <s660762@student.roundrockisd.org>
2014-11-04 15:40:11 -0600

    lab12

--- 85pt.py
+++ 85pt.py
@@ -29,6 +29,20 @@ class MyApp:
 			self.button1["background"] = "yellow"
 		else:
 			self.button1["background"] = "green"
+			
+		
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="Cancel", background= "red")
+		self.button2.grid(row=0, column=1)	
+		
+		self.button2.bind("<Button-1>", self.button2Click) 
+		
+		
+	def button2Click(self, event):    
+		if self.button2["background"] == "red": 
+			self.button2["background"] = "blue"
+		else:
+			self.button2["background"] = "red"
 	
 		
 root = Tk()
